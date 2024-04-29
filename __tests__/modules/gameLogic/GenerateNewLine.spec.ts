e4c20ff3562f6784e376fc9e4148889ba8aba50c
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-04-29 18:51:45 +0300

    feat: unit tests GenerateNewLine

---
+++ __tests__/modules/gameLogic/GenerateNewLine.spec.ts
@@ -0,0 +1,18 @@
+import { generateNewLine } from "../../../src/modules/gameLogic"
+import { BoxColors } from "../../../src/constants/constants";
+
+describe('Generate New Line', () => {
+    test('Generate a new array with a length of 10', () => {
+        const arr = generateNewLine();
+
+        expect(arr).toBeInstanceOf(Array);
+        expect(arr.length).toEqual(10);
+    })
+
+    test('All elements of array must be strings from BoxColors', () => {
+        const arr = generateNewLine();
+        const boxColors: string[] = Object.values(BoxColors);
+
+        arr.forEach(rgb => expect(boxColors.includes(rgb)).toBeTruthy());
+    })
+})
