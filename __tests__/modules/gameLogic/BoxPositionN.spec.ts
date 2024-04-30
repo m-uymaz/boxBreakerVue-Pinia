3621229cb374cc036a83fb5cefd84ab775e3b3b7
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-04-30 11:09:31 +0300

    feat: unit tests BoxPositionN

---
+++ __tests__/modules/gameLogic/BoxPositionN.spec.ts
@@ -0,0 +1,30 @@
+import { GRID_BOXES_SIZE, GridColumnsIndices, GridRowIndices } from "../../../src/constants/constants";
+import { boxPositionN } from "../../../src/modules/gameLogic"
+
+describe('BoxPositionN', () => {
+    it('returns fist grid box number', () => {
+        const firstBox = 1;
+        const indices = { y: 0, x: 0 }
+
+        const boxN = boxPositionN(indices.y, indices.x);
+
+        expect(boxN).toEqual(firstBox)
+    })
+
+    it('returns last possible grid box number', () => {
+        const indices = { y: GridRowIndices.Last, x: GridColumnsIndices.Last }
+
+        const boxN = boxPositionN(indices.y, indices.x);
+
+        expect(boxN).toEqual(GRID_BOXES_SIZE)
+    })
+
+    it('returns the 75th box', () => {
+        const targetBox = 75;
+        const indices = { y: 7, x: 4 }
+
+        const boxN = boxPositionN(indices.y, indices.x);
+
+        expect(boxN).toEqual(targetBox)
+    })
+})
