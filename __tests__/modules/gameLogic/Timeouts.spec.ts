10697692e75821fbe5d4c7594fe0ee65dd846d30
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-04-30 16:43:48 +0300

    feat: unit tests Timeout

--- __tests__/modules/gameLogic/Timeouts.spec.ts
+++ __tests__/modules/gameLogic/Timeouts.spec.ts
@@ -1,38 +1,51 @@
 import { setActivePinia, createPinia } from "pinia";
-import { explodeDelay } from "../../../src/modules/gameLogic";
+import { explodeDelay, clearPrevTimeouts } from "../../../src/modules/gameLogic";
 import { useAppStateStore } from "../../../src/stores/appStateStore";
 
 describe("Timeouts", () => {
-  beforeEach(() => {
-    // creates a fresh pinia and makes it active
-    // so it's automatically picked up by any useStore() call
-    // without having to pass it to it: `useStore(pinia)`
+    beforeEach(() => {
+        // creates a fresh pinia and makes it active
+        // so it's automatically picked up by any useStore() call
+        // without having to pass it to it: `useStore(pinia)`
     setActivePinia(createPinia());
-  });
+    });
 
-  jest.useFakeTimers();
-  jest.spyOn(global, "setTimeout");
+    jest.useFakeTimers();
+    jest.spyOn(global, "setTimeout");
 
-  test("...", () => {
-    const store = useAppStateStore();
-    const seedArrBlinkingBoxes = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];  
-
-    const timeout = explodeDelay(store, 1000);
-
-    store.blinkingBoxesN = [...seedArrBlinkingBoxes];
-
-    expect(store.blinkingBoxesN).toEqual(seedArrBlinkingBoxes);
-
-    const callBack = jest.fn(() => timeout);
-
-    callBack();
-    jest.advanceTimersByTime(500);
+    test('explodingBoxeN takes arr values of blinkingBoxesN and blinkingBoxesN is left with no values in arr', () => {
+        const store = useAppStateStore();
+        const seedArrBlinkingBoxes = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];  
+        const timeoutFunc = jest.fn(() => explodeDelay(store, 1000));
     
-    expect(store.blinkingBoxesN).toEqual(seedArrBlinkingBoxes);
+        store.blinkingBoxesN = [...seedArrBlinkingBoxes];
+        expect(store.blinkingBoxesN).toEqual(seedArrBlinkingBoxes);
+        expect(timeoutFunc).not.toHaveBeenCalled();  
 
-    jest.advanceTimersByTime(500);
+        timeoutFunc();
+        jest.advanceTimersByTime(500);
     
-    expect(callBack).toHaveBeenCalledTimes(1);
-    expect(store.blinkingBoxesN).toEqual([]);
-  });
+        expect(store.blinkingBoxesN).toEqual(seedArrBlinkingBoxes);
+        expect(store.explodingBoxesN).toEqual([]);
+
+        jest.advanceTimersByTime(500);
+    
+        expect(timeoutFunc).toHaveBeenCalledTimes(1);
+        expect(store.blinkingBoxesN).toEqual([]);
+        expect(store.explodingBoxesN).toEqual(seedArrBlinkingBoxes);
+    });
+    
+    test('clearing active timeouts', () => {
+        const store = useAppStateStore();
+        const timeoutFunc = jest.fn(() => explodeDelay(store, 1000));
+
+        timeoutFunc();
+        jest.advanceTimersByTime(500);
+
+        expect(typeof store.timeouts.explodeTimeout).toEqual('object');
+
+        clearPrevTimeouts(store);
+
+        expect(store.timeouts.explodeTimeout).toBeNull();
+    })
 });
