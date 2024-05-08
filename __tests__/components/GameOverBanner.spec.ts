5824aac139fa8e344fb1b0547b32aeeefae50ce3
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-05-08 13:46:01 +0300

    feat: unit test RightNav

--- __tests__/components/GameOverBanner.spec.ts
+++ __tests__/components/GameOverBanner.spec.ts
@@ -1,12 +1,27 @@
-import {mount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import GameOverBanner from '../../src/components/GameOverBanner.vue'
 
-test('displays message', () => {
-  const wrapper = mount(GameOverBanner, {
-    props: {
-      isGameOver: false
-    }
-  })
-    
-    expect(wrapper.props().isGameOver).toBeFalsy();
-})
+describe('GameOverBanner', () => {
+    test('displays game over message', () => {
+        const wrapper = shallowMount(GameOverBanner, {
+            props: {
+                isGameOver: false
+            }
+        });
+
+        expect(wrapper.props().isGameOver).toBe(false);
+        expect(wrapper.find('.h1-none').exists()).toBe(true);
+    });
+
+    test('does not display game over message', () => {
+        const wrapper = shallowMount(GameOverBanner, {
+            props: {
+                isGameOver: true
+            }
+        });
+
+        expect(wrapper.props().isGameOver).toBeTruthy();
+        expect(wrapper.find('.h1-blinking').exists()).toBe(true);
+        expect(wrapper.find('.h1-none').exists()).toBe(false);
+    });
+});
---
+++ __tests__/components/RightNav.spec.ts
@@ -0,0 +1,59 @@
+
+import { setActivePinia, createPinia } from 'pinia';
+import {createTestingPinia} from '@pinia/testing'
+import { shallowMount } from '@vue/test-utils';
+import RightNav from '../../src/components/RightNav.vue'
+import { useAppStateStore } from '../../src/stores/appStateStore';
+
+describe('RightNav', () => {
+    let wrapper;
+    let store;
+    
+    beforeEach(() => {
+        setActivePinia(createPinia())
+        store = useAppStateStore();
+        
+        wrapper = shallowMount(RightNav, {
+            global: {
+                plugins: [createTestingPinia()],
+                mocks: {
+                    $store: store
+                },
+            },
+        });
+    });
+
+    Object.defineProperty(global, 'performance', {
+        writable: true,
+    });
+
+    jest.useFakeTimers();
+    jest.spyOn(global, "setInterval");
+
+    it('starts the fall when fallOn() is called', () => {
+        const spy = jest.spyOn(wrapper.vm, 'fallOn');
+
+        wrapper.vm.fallOn();
+
+        expect(spy).toHaveBeenCalled();
+        
+        jest.advanceTimersByTime(2500);
+
+        expect(wrapper.vm.store.interval).not.toBeNull();
+    });
+
+    it('stops the fall when fallOff() is called', () => {
+        const spy = jest.spyOn(wrapper.vm, 'fallOn');
+
+        wrapper.vm.fallOn();
+
+        expect(spy).toHaveBeenCalled();
+        
+        jest.advanceTimersByTime(2500);
+
+        expect(wrapper.vm.store.interval).not.toBeNull();
+
+        wrapper.vm.fallOff();
+        expect(wrapper.vm.store.interval).toBeNull();
+    })
+});
