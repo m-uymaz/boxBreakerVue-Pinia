c4dfbd66d85d8c7df19e4e811eac3bf3f12d0352
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-05-08 13:46:27 +0300

    feat: unit test RightNav

--- __tests__/modules/gameLogic/Timeouts.spec.ts
+++ __tests__/modules/gameLogic/Timeouts.spec.ts
@@ -4,9 +4,6 @@ import { useAppStateStore } from "../../../src/stores/appStateStore";
 
 describe("Timeouts", () => {
     beforeEach(() => {
-        // creates a fresh pinia and makes it active
-        // so it's automatically picked up by any useStore() call
-        // without having to pass it to it: `useStore(pinia)`
     setActivePinia(createPinia());
     });
 
