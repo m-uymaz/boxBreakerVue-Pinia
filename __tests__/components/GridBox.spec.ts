90774b56d2b63866300011fe662debcba8268168
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-05-14 11:07:31 +0300

    refactor GridBox unit tests

--- __tests__/components/GridBox.spec.ts
+++ __tests__/components/GridBox.spec.ts
@@ -1,18 +1,12 @@
-import { setActivePinia, createPinia} from 'pinia';
-import { useAppStateStore } from '../../src/stores/appStateStore';
-import { shallowMount, VueWrapper} from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import GridBox from '../../src/components/GridBox.vue';
-import { AppStore } from '../../src/types/types';
 import { ALICEBLUE } from '../../src/constants/constants';
 
 describe('GridBox', () => {
     let wrapper;
-    let store: AppStore;
 
     beforeEach(() => {
-        setActivePinia(createPinia());
-        store = useAppStateStore();
         wrapper = shallowMount(GridBox, {
             props: {
                 boxN: 1
@@ -26,7 +20,7 @@ describe('GridBox', () => {
     test('all column boxes with "arrowIndex" index have .box-selected class', () => {
         let testBoxN = 6;
 
-        store.gridArray.forEach(async () => {
+        wrapper.vm.store.gridArray.forEach(async () => {
             await wrapper.setProps({ boxN: testBoxN });
 
             expect(wrapper.find('.box-selected').exists()).toBe(true);
@@ -38,7 +32,7 @@ describe('GridBox', () => {
     test('column boxes NOT with "arrowIndex" index, have .box class', () => {
         let testBoxN = 10;
 
-        store.gridArray.forEach(async () => {
+        wrapper.vm.store.gridArray.forEach(async () => {
             await wrapper.setProps({ boxN: testBoxN });
 
             expect(wrapper.find('.box-selected').exists()).not.toBe(true);
