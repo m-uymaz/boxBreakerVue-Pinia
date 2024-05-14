a0fed43a631270361e005c30724662f97dc4b530
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-05-14 11:00:40 +0300

    refactor GameOverBanner

--- __tests__/components/GameOverBanner.spec.ts
+++ __tests__/components/GameOverBanner.spec.ts
@@ -2,23 +2,24 @@ import {shallowMount} from '@vue/test-utils'
 import GameOverBanner from '../../src/components/GameOverBanner.vue'
 
 describe('GameOverBanner', () => {
-    test('displays game over message', () => {
-        const wrapper = shallowMount(GameOverBanner, {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(GameOverBanner, {
             props: {
                 isGameOver: false
             }
         });
+    });
+
+    test('displays game over message', async () => {
+        await wrapper.setProps({ isGameOver: false });
 
         expect(wrapper.props().isGameOver).toBe(false);
         expect(wrapper.find('.h1-none').exists()).toBe(true);
     });
 
-    test('does not display game over message', () => {
-        const wrapper = shallowMount(GameOverBanner, {
-            props: {
-                isGameOver: true
-            }
-        });
+    test('does not display game over message', async () => {
+        await wrapper.setProps({ isGameOver: true });
 
         expect(wrapper.props().isGameOver).toBeTruthy();
         expect(wrapper.find('.h1-blinking').exists()).toBe(true);
