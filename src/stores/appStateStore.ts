5deae909682aae19bde2a81bd570baa2aa76bb4b
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-05-16 10:40:17 +0300

    refactor

--- src/stores/appStateStore.ts
+++ src/stores/appStateStore.ts
@@ -54,24 +54,24 @@ export const useAppStateStore = defineStore('appStateStore', {
                 direction === KeyboardInputs.ArrowLeft && this.arrowIndex === GridColumnsIndices.First
                 ||
                 direction === KeyboardInputs.ArrowRight && this.arrowIndex === GridColumnsIndices.Last
-            ) return;
+            ) return
 
-            this.gridArray[GridRowIndices.Last][this.arrowIndex] = null;
+            this.gridArray[GridRowIndices.Last][this.arrowIndex] = null
 
-            direction === KeyboardInputs.ArrowRight ? this.arrowIndex++ : this.arrowIndex--;
-            this.gridArray[GridRowIndices.Last][this.arrowIndex] = ARROW;
+            direction === KeyboardInputs.ArrowRight ? this.arrowIndex++ : this.arrowIndex--
+            this.gridArray[GridRowIndices.Last][this.arrowIndex] = ARROW
         },
         catchBox(): void {
             for (let y = GridRowIndices.NextToLast; 0 <= y; y--) {
-                if (this.explodedBoxes.filter(obj => obj.y === y && obj.x === this.arrowIndex).length) break;
+                if (this.explodedBoxes.filter(obj => obj.y === y && obj.x === this.arrowIndex).length) break
 
                 if (this.gridArray[y][this.arrowIndex] === null) {
-                    continue;
+                    continue
                 } else {
-                    this.caughtBox = this.gridArray[y][this.arrowIndex];
+                    this.caughtBox = this.gridArray[y][this.arrowIndex]
                     this.gridArray[y][this.arrowIndex] = null;
                     this.caughtBoxFrom = { y: y, x: this.arrowIndex }
-                    break;
+                    break
                 }
             }
         },
@@ -80,55 +80,55 @@ export const useAppStateStore = defineStore('appStateStore', {
                 // If you try to put a block on the last index line - Game Over
                 if (yIndex === GridRowIndices.NextToLast && this.gridArray[yIndex][this.arrowIndex] !== null) {
                     this.setGameOver()
-                    break;
+                    break
                 };
 
                 if (yIndex === GridRowIndices.First) {
                     if (this.gridArray[yIndex][this.arrowIndex] === null) {
-                        this.gridArray[yIndex][this.arrowIndex] = this.caughtBox;
+                        this.gridArray[yIndex][this.arrowIndex] = this.caughtBox
                         this.thrownBox = { y: 0, x: this.arrowIndex }
                     } else {
-                        this.gridArray[yIndex + 1][this.arrowIndex] = this.caughtBox;
+                        this.gridArray[yIndex + 1][this.arrowIndex] = this.caughtBox
                         this.thrownBox = { y: 1, x: this.arrowIndex }
                     }
-                    this.caughtBox = null;
+                    this.caughtBox = null
                 }
-                if (this.gridArray[yIndex][this.arrowIndex] === null) continue;
+                if (this.gridArray[yIndex][this.arrowIndex] === null) continue
 
                 if (yIndex !== GridRowIndices.First) {
                     //Put the box on the next Y index
-                    let upperGridIndex: number = yIndex + 1;
-                    this.gridArray[upperGridIndex][this.arrowIndex] = this.caughtBox;
+                    let upperGridIndex: number = yIndex + 1
+                    this.gridArray[upperGridIndex][this.arrowIndex] = this.caughtBox
 
                     this.thrownBox = { y: yIndex + 1, x: this.arrowIndex }
                     this.caughtBox = null;
                 }
 
                 //When the box is put, find if it creates a hight
-                if (yIndex >= this.highestPositionY) this.highestPositionY++;
+                if (yIndex >= this.highestPositionY) this.highestPositionY++
 
                 if (this.thrownBox) {
-                    this.checkBoxPositions = [{ y: this.thrownBox.y, x: this.thrownBox.x }];
-                    this.caughtBoxFrom = null;
+                    this.checkBoxPositions = [{ y: this.thrownBox.y, x: this.thrownBox.x }]
+                    this.caughtBoxFrom = null
                 }
                 break;
             }
         },
         moveDown(): void {
-            if (this.gameOverState) return;
+            if (this.gameOverState) return
 
-            if (this.checkBoxPositions.length) this.checkBoxPositions.map(position => position.y++);
-            if (this.explodedBoxes.length) this.explodedBoxes.map(position => position.y++);
-            if (this.blinkingBoxesN.length) this.blinkingBoxesN = this.blinkingBoxesN.map(boxN => boxN += 10);
+            if (this.checkBoxPositions.length) this.checkBoxPositions.map(position => position.y++)
+            if (this.explodedBoxes.length) this.explodedBoxes.map(position => position.y++)
+            if (this.blinkingBoxesN.length) this.blinkingBoxesN = this.blinkingBoxesN.map(boxN => boxN += 10)
     
-            const newArray: newLineArray = generateNewLine();
-            this.gridArray.pop();
-            this.gridArray.unshift(newArray);
+            const newArray: newLineArray = generateNewLine()
+            this.gridArray.pop()
+            this.gridArray.unshift(newArray)
 
-            let isUpperRowBlank: boolean = this.gridArray[this.highestPositionY + 1].every(box => box === null);
+            let isUpperRowBlank: boolean = this.gridArray[this.highestPositionY + 1].every(box => box === null)
 
-            if (!isUpperRowBlank) this.highestPositionY++;
-            if (this.highestPositionY + 1 === GridLengths.RowLength) this.setGameOver();
+            if (!isUpperRowBlank) this.highestPositionY++
+            if (this.highestPositionY + 1 === GridLengths.RowLength) this.setGameOver()
         },
         gameOverBoxDance() {
             bottomHorizontalLToR(this.gridArray)
